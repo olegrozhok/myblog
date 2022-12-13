01a326a4edfbbdc37289e34d9d8b7698e53f5c31
olegrozhok <oleg.yur.rozhok@gmail.com>
2022-12-13 14:20:44 +0300

    adding comments to post, also appears ability to edit and delete comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
@@ -73,3 +73,32 @@ def post_remove_draft(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_draft_list')
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comments = Comment.objects.filter()
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form, 'comments': comments})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
