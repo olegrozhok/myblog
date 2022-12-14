59fa39ddf7cdcbf680b58500dcbd24ebd0a5ace4
olegrozhok <oleg.yur.rozhok@gmail.com>
2022-12-14 11:00:42 +0300

    adding signup for our site, change main font

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    re_path(r'^signup/$', views.signup, name='signup'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post, Comment
-from .forms import PostForm, CommentForm
-from django.shortcuts import redirect
+from .forms import PostForm, CommentForm, SignUpForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+
+
+@login_required
+def home(request):
+    return render(request, '/')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.profile.birth_date = form.cleaned_data.get('birth_date')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 def post_list(request):
